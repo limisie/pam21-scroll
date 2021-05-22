9bace25394e2394a256c7f0dc04a5e9336ffac90
Kaja Limisiewicz <k.limisiewicz@gmail.com>
2021-05-22 18:19:04 +0200

    feat: scroll links

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
 const root = document.getElementById('root');
+const boxLinks = document.getElementById('box-links');
+
 const images = 182;
 const labels = [64, 93, 125];
 const labelDisplay = 5;
 
-for (let i = 0; i < images; i++) {
+for (var i = 0; i < images; i++) {
     const child = document.createElement('img');
 
     const num = i.toString().length === 1 ? `000${i}` : i.toString().length === 2 ? `00${i}` : `0${i}`;
     child.src = `./img/${num}.png`;
-    child.classList = `image image-${i}`;
+    child.id = `img-${i}`;
+    if (labels.includes(i)) {
+        child.classList = `image image-${i} img-labeled`;
+    } else {
+        child.classList = `image image-${i}`;
+    }
     root.appendChild(child);
 }
 
-const show = (i) => {
+const scrollLink = (props) => {
+    return () => { window.scroll({ top: props.y, behavior: 'smooth' }); }
+}
+
+for (var i = 0; i < labels.length; i++) {
+    const boxLink = document.createElement('div');
+    boxLink.id = `box-link-${labels[i]}`
+    boxLink.classList = `box-link box-link-${labels[i]}`
+    boxLinks.appendChild(boxLink);
+    boxLink.addEventListener('click', scrollLink({ y: labels[i] * 10 }));
+}
+
+document.getElementById('label').addEventListener('click', scrollLink({ y: 0 }));
+
+
+const handleZoom = (i) => {
+    const bls = document.querySelector(`#box-links`);
+    bls.style.display = 'none';
+
+    if (i == 0 || i == images - 1) {
+        bls.style.display = 'block';
+    }
+}
+
+const handleScroll = (i) => {
     document.querySelectorAll('.image').forEach(elem => elem.style.opacity = 0);
     document.querySelector(`.image-${i}`).style.opacity = 1;
+}
+
+const handleLabels = (i) => {
     document.querySelector(`.label`).style.opacity = 0;
     for (const label of labels) {
         if (label - labelDisplay < i & i < label + labelDisplay) {
             const diff = labelDisplay - Math.abs(i - label);
-            console.log(label);
             document.querySelector(`.label`).style.opacity = 0.15 * diff;
         }
     }
+}
+
+const show = (i) => {
+    handleZoom(i);
+    handleScroll(i);
+    handleLabels(i);
 }
 
 window.onscroll = () => {
